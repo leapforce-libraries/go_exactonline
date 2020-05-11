5901a563d3273b54a0103980d5e8abe27054f386
caselongo <caselongo@gmail.com>
2020-05-11 11:46:38 +0200

    x

--- item.go
+++ item.go
@@ -7,11 +7,53 @@ import (
 	types "github.com/Leapforce-nl/go_types"
 )
 
+// Item stores Item from exactonline
+//
 type Item struct {
-	ID   types.GUID `json:"ID"`
-	Code string     `json:"Code"`
+	ID          types.GUID `json:"ID"`
+	Code        string     `json:"Code"`
+	Description string     `json:"Description"`
 }
 
+func (eo *ExactOnline) GetItemsInternal(filter string) (*[]Item, error) {
+	selectFields := GetJsonTaggedFieldNames(Item{})
+	urlStr := fmt.Sprintf("%s%s/logistics/Items?$select=%s", eo.ApiUrl, strconv.Itoa(eo.Division), selectFields)
+	if filter != "" {
+		urlStr += fmt.Sprintf("&$filter=%s", filter)
+	}
+	//fmt.Println(urlStr)
+
+	items := []Item{}
+
+	for urlStr != "" {
+		its := []Item{}
+
+		str, err := eo.Get(urlStr, &its)
+		if err != nil {
+			fmt.Println("ERROR in GetItemsInternal:", err)
+			fmt.Println("url:", urlStr)
+			return nil, err
+		}
+
+		items = append(items, its...)
+
+		urlStr = str
+		//urlStr = ""
+	}
+
+	return &items, nil
+}
+
+func (eo *ExactOnline) GetItems() (*[]Item, error) {
+	acc, err := eo.GetItemsInternal("")
+	if err != nil {
+		return nil, err
+	}
+
+	return acc, nil
+}
+
+/*
 func (eo *ExactOnline) GetItems() error {
 	selectFields := GetJsonTaggedFieldNames(Item{})
 	urlStr := fmt.Sprintf("%s%s/logistics/Items?$select=%s", eo.ApiUrl, strconv.Itoa(eo.Division), selectFields)
@@ -33,4 +75,4 @@ func (eo *ExactOnline) GetItems() error {
 	}
 
 	return nil
-}
+}*/
